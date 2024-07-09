bf8ea8519ebe92c3312b98ecfe7b63dbb5ddc080
elcous <a.hedrevich@gmail.com>
2024-07-09 01:24:02 -0600

    Add diff preview

---
+++ app/tools/code_diff.js
@@ -0,0 +1,28 @@
+const { Diff2HtmlUI } = require('diff2html/lib/ui/js/diff2html-ui');
+const diff = require('diff');
+
+function generateDiff(oldContent, newContent, oldFileName, newFileName) {
+  const patch = diff.createTwoFilesPatch(oldFileName, newFileName, oldContent, newContent);
+  return patch;
+}
+
+function drawDiff(targetElement, diffString) {
+  const diffConfig = {
+    drawFileList: false,
+    highlight: true,
+    outputFormat: 'side-by-side',
+    matching: 'lines',
+    colorScheme: chatController.settings.theme,
+    showDiffOnly: true,
+    fileContentToggle: false,
+    synchronisedScroll: true,
+  };
+  const diff2htmlUi = new Diff2HtmlUI(targetElement, diffString, diffConfig);
+  diff2htmlUi.draw();
+  diff2htmlUi.highlightCode();
+}
+
+module.exports = {
+  generateDiff,
+  drawDiff,
+};
